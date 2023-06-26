dbf9970398914ce49628692d882dfad769147744
GAD KAMAU <gadkamau46@gmail.com>
2023-06-26 21:10:21 +0300

    Added if/else statements

---
+++ learnPrograming/scrimba qiz/blackJack/index.js
@@ -0,0 +1,21 @@
+let firstCard = 6
+
+let secondCard = 19
+
+let sum = firstCard + secondCard
+
+let hasBlackJack = false
+
+let isAlive = true
+
+if (sum <= 20) {
+    console.log("Do you want to draw a new card? 🙂")
+}else if (sum === 21) {
+    console.log("Wohoo! You've got Blackjack! 🥳")
+    hasBlackJack = true
+}else {
+   console.log("You're out of the game! 😭")
+   isAlive = false 
+}
+
+console.log(isAlive)
